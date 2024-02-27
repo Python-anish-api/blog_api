77eb3aff0cc96a27e27477a5904e65ffcfc41c3b
Anish Gharti <anishchengre@kali.chengre>
2024-02-27 20:49:19 +0545

    schemas crated

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-
+from database import models
+from database.database import engine
 app = FastAPI()
 
 @app.get('/')
 def hello_world():
     return {'message': 'Hello World'}
 
+
+models.Base.metadata.create_all(engine)
